092675035e134ffaad7840d75f4ed3fa972c9079
DrDano <danielharned@gmail.com>
2022-08-13 09:36:16 -0700

    inital commit #time 4h

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php require_once('../private/initialize.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GBI - <?php echo $page_title ?? 'Home'; ?></title>
+</head>
+<body>
+    <?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+    <div id="content">
+    </div>
+
+    <?php include(SHARED_PATH . '/staff_footer.php'); ?>
+</body>
+</html>
