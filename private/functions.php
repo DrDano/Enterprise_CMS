21593904e03b856ecb4279ddfde8f5097310744d
DrDano <danielharned@gmail.com>
2022-08-15 20:08:11 -0700

    adding ability to edit subjects #time 30m

--- private/functions.php
+++ private/functions.php
@@ -33,4 +33,12 @@
         exit;
     }
 
+    function is_post_request() {
+        return $_SERVER['REQUEST_METHOD'] == 'POST';
+    }
+
+    function is_get_request() {
+        return $_SERVER['REQUEST_METHOD'] == 'GET';
+    }
+
 ?>
