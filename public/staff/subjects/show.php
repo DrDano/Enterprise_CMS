82a6386d8dd53c9caf8f3586f5385dfede600266
DrDano <danielharned@gmail.com>
2022-08-22 09:38:34 -0700

    adding single record query to show subject page #time 30m

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,18 +1,41 @@
 <?php require_once('../../../private/initialize.php'); ?>
 <div id="content">
-<?php 
+    <?php
 
     $id = $_GET['id'] ?? '1';
     $subject_title = $_GET['subject'] ?? 'Subject';
 
+    $subject = find_subject_by_id($id);
+
     $page_title = $subject_title;
     include(SHARED_PATH . '/staff_header.php');
 
-?>
-<div class="subject show">
-    <?php echo "Subject ID: " . h($id); ?>
-</div>
+    ?>
+    <div class="subject show">
+        <?php echo "Subject ID: " . h($id); ?>
+    </div>
     </br><a href='../subjects/'>&laquo; Back to List</a>
+
+    <div class="subject show">
+
+        <h1>Subject: <?php echo h($subject['menu_name']); ?></h1>
+
+        <div class="attributes">
+            <dl>
+                <dt>Menu Name</dt>
+                <dd><?php echo h($subject['menu_name']); ?></dd>
+            </dl>
+            <dl>
+                <dt>Position</dt>
+                <dd><?php echo h($subject['position']); ?></dd>
+            </dl>
+            <dl>
+                <dt>Visible</dt>
+                <dd><?php echo $subject['visible'] == '1' ? 'true' : 'false'; ?></dd>
+            </dl>
+        </div>
+
+    </div>
 </div>
 
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>
